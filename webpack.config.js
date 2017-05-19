e899e6e8eafbcb318b1171ee32539b469b52ccc0
Olov Nordblad <Olle@Olovs-MacBook-Air.local>
2017-05-19 13:05:30 +0200

    Trying to fix Azure Webpack error

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,11 @@ module.exports = (env) => {
         module: {
             rules: [
                 { test: /\.vue$/, include: /ClientApp/, use: 'vue-loader' },
-                { test: /\.js$/, include: /ClientApp/, use: 'babel-loader' },
+                {
+                    test: /\.js$/, include: /ClientApp/, use: 'babel-loader', query: {
+                        presets: ["es2015"]
+                    }
+                },
                 { test: /\.ts$/, include: /ClientApp/, use: 'awesome-typescript-loader?silent=true' },
                 { test: /\.css$/, use: isDevBuild ? ['style-loader', 'css-loader'] : ExtractTextPlugin.extract({ use: 'css-loader' }) },
                 { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
